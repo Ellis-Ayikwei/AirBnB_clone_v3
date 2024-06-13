4ff5e463626b1d9907e3b237588a13b4251371d6
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-06-13 02:05:13 +0000

    created the not found and the stutus file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -10,6 +10,24 @@ app = Flask(__name__)
 CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
 
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    Return JSON formatted 404 status code response.
+
+    Args:
+        error (Exception): The error that raised the 404.
+
+    Returns:
+        Flask response: A JSON response with a 404 status code and a
+        message indicating that the requested resource was not found.
+    """
+    # Create a JSON response with a 404 status code and a message.
+    response = make_response(jsonify({'error': 'Not found'}), 404)
+
+    return response
 
 @app.teardown_appcontext
 def teardown_db(self):
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from  api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 @app_views.route("/status")
 def status():
@@ -17,3 +18,18 @@ def status():
 
     # Convert the dictionary to a JSON object and return it.
     return jsonify(return_dict)
+
+@app_views.route("/stats")
+def storage_counts():
+    '''
+        return counts of all classes in storage
+    '''
+    cls_counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(cls_counts)
